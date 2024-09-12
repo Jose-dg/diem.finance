499782244f2e2b15118e6e042255f304c8947c5b
jose <jose.ojeda.k@hotmail.com>
2024-09-12 11:46:08 -0500

    dashboard

---
+++ apps/dashboard/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import CreditDetailAPIView, FinanceView, NewCreditsAPIView, PendingCreditsAPIView, TransactionsAPIView, ClientsWithDefaultAPIView, FinancialCreditsAPIView, ProductCreditsAPIView
+
+urlpatterns = [
+    path('finance/', FinanceView.as_view(), name='finance'),
+    path('transactions/', TransactionsAPIView.as_view(), name='transactions'),
+    path('credits/new/', NewCreditsAPIView.as_view(), name='new_credits'),
+    path('credits/pending/', PendingCreditsAPIView.as_view(), name='pending_credits'),
+    path('clients/defaults/', ClientsWithDefaultAPIView.as_view(), name='clients_with_default'),
+    path('credits/financial/', FinancialCreditsAPIView.as_view(), name='financial_credits'),
+    path('credits/products/', ProductCreditsAPIView.as_view(), name='product_credits'),
+    path('credit/', CreditDetailAPIView.as_view(), name='credit-detail'),
+]
+
