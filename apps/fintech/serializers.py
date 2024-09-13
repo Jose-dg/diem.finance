d8331b3da01024fc16e065cc3d189d11607dab79
jose <jose.ojeda.k@hotmail.com>
2024-09-12 19:24:34 -0500

    Runing dashboard

--- apps/fintech/serializers.py
+++ apps/fintech/serializers.py
@@ -4,7 +4,15 @@ from .models import *
 
 User = get_user_model()
 
+class PhoneNumberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhoneNumber
+        fields = ['country_code', 'phone_number']
+
 class UserSerializer(serializers.ModelSerializer):
+
+    phone_1 = PhoneNumberSerializer() 
+
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'phone_1']  # Ajusta los campos según tus necesidades
@@ -30,13 +38,13 @@ class TransactionSerializer(serializers.ModelSerializer):
         fields = ['uid', 'transaction_type', 'account', 'date', 'amount', 'currency', 'method_amount']
 
 class CreditSerializer(serializers.ModelSerializer):
-    client = UserSerializer()  # Relación con el modelo User
+    user = UserSerializer()  # Relación con el modelo User
     payments = AccountMethodAmountSerializer(many=True, read_only=True)  # Lista de pagos relacionados con el crédito
 
     class Meta:
         model = Credit
         fields = [
-            'uid', 'client', 'cost', 'price', 'currency', 'total_abonos', 'pending_amount', 
+            'uid', 'user', 'cost', 'price', 'currency', 'total_abonos', 'pending_amount', 
             'first_date_payment', 'second_date_payment', 'credit_days', 'payments'
         ]
 
@@ -47,3 +55,25 @@ class CreditSerializer(serializers.ModelSerializer):
         instance.pending_amount = instance.price - total_abonos
         instance.save()
         return instance
+
+
+class CreditDetailSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True) 
+    abonos = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Credit
+        fields = [
+            'uid', 'state', 'credit_type', 'cost', 'price', 'earnings',
+            'first_date_payment', 'second_date_payment', 'credit_days',
+            'description', 'interest', 'refinancing', 'total_abonos',
+            'pending_amount', 'installment_number', 'installment_value',
+            'is_in_default', 'created_at', 'updated_at', 'morosidad_level',
+            'category', 'user', 'currency', 'periodicity', 'payment', 'registered_by'
+        ]
+        # fields = ['uid', 'credit_type', 'client_name', 'price', 'first_date_payment', 'total_abonos', 'pending_amount', 'abonos']
+
+    def get_abonos(self, obj):
+        # Verificar si el crédito tiene pagos asociados
+        abonos = obj.payments.all() if obj.payments.exists() else []
+        return AccountMethodAmountSerializer(abonos, many=True).data
--- apps/fintech/urls.py
+++ apps/fintech/urls.py
@@ -1,22 +1,40 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
+from django.urls import path
 from .views import (
-    ClientViewSet,
-    CategoryViewSet,
+    UserViewSet,
     AccountViewSet,
     TransactionViewSet,
     CreditViewSet,
-    ExpenseViewSet
+    FinanceView,
+    CreditsAPIView,
+    PendingCreditsAPIView,
+    TransactionsAPIView,
+    ClientsWithDefaultAPIView,
+    ProductCreditsAPIView,
+    CreditDetailAPIView,
+    FinancialCreditsAPIView
 )
 
-router = DefaultRouter()
-router.register(r'clients', ClientViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'accounts', AccountViewSet)
-router.register(r'transactions', TransactionViewSet)
-router.register(r'credits', CreditViewSet)
-router.register(r'expenses', ExpenseViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    # Rutas para ViewSets
+    path('clients/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='client-list'),
+    path('clients/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='client-detail'),
+        
+    path('accounts/', AccountViewSet.as_view({'get': 'list', 'post': 'create'}), name='account-list'),
+    path('accounts/<int:pk>/', AccountViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='account-detail'),
+    
+    path('transactions/', TransactionViewSet.as_view({'get': 'list', 'post': 'create'}), name='transaction-list'),
+    path('transactions/<int:pk>/', TransactionViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='transaction-detail'),
+    
+    path('credits/', CreditViewSet.as_view({'get': 'list', 'post': 'create'}), name='credit-list'),
+    path('credits/<int:pk>/', CreditViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='credit-detail'),
+        
+    # Rutas para APIViews
+    path('data/', FinanceView.as_view(), name='finance-summary'),
+    path('credits/total/', CreditsAPIView.as_view(), name='credits'),
+    path('credits/pending/', PendingCreditsAPIView.as_view(), name='pending-credits'),
+    path('transactions/period/', TransactionsAPIView.as_view(), name='transactions-period'),
+    path('clients/defaults/', ClientsWithDefaultAPIView.as_view(), name='clients-default'),
+    path('credits/product/', ProductCreditsAPIView.as_view(), name='product-credits'),
+    path('credits/detail/', CreditDetailAPIView.as_view(), name='credit-detail'),
+    path('credits/financial/', FinancialCreditsAPIView.as_view(), name='financial-credits'),
 ]
