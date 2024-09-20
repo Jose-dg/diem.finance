733f8dff23941949263eb825f4b58d9392f2eea8
jose <jose.ojeda.k@hotmail.com>
2024-09-19 20:23:08 -0500

    Ajustes en el modelo Expense y corrección de errores

--- core/urls.py
+++ core/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('fintech/', include('apps.fintech.urls')),
     path('dashboard/', include('apps.dashboard.urls')),
 
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
