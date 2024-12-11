22fd11cc058a44e9efb08dbc4ef2bce43a107d86
jose <jose.ojeda.k@hotmail.com>
2024-12-10 23:33:13 -0500

    Updating for create transaction from frontend

--- apps/fintech/views.py
+++ apps/fintech/views.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+import uuid
 from rest_framework import viewsets
 from .models import User, Account, Transaction, Credit, Expense
 from .serializers import (
@@ -12,8 +14,10 @@ from django.db.models import Sum
 from datetime import datetime, timedelta
 from django.utils import timezone  
 from .serializers import CreditSerializer, TransactionSerializer
-from .models import User, Credit, Transaction, Expense, AccountMethodAmount
+from .models import User, Credit, Transaction, Expense, AccountMethodAmount, Transaction
 from django.utils.dateparse import parse_date
+from django.shortcuts import get_object_or_404
+from decimal import Decimal
 
 from .models import (
     AccountMethodAmount,
@@ -33,6 +37,47 @@ class AccountViewSet(viewsets.ModelViewSet):
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Extraer datos del request
+        credit_uid = request.data.get("credit_uid")
+        amount = Decimal(request.data.get("amount"))
+        description = request.data.get("description")
+        user_id = request.data.get("user_id")
+        category_id = request.data.get("category_id")
+        payment_type = request.data.get("payment_type")
+
+        # Validar el crédito
+        credit = get_object_or_404(Credit, uid=credit_uid)
+        if amount <= 0:
+            return Response(
+                {"detail": "El monto debe ser mayor a 0"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # Crear la transacción
+        transaction = Transaction.objects.create(
+            transaction_type="income",
+            description=description or f"Pago registrado: {payment_type}",
+            user_id=user_id,
+            category_id=category_id,
+        )
+
+        # Crear el abono (AccountMethodAmount)
+        AccountMethodAmount.objects.create(
+            payment_method=credit.payment,
+            payment_code=str(uuid.uuid4()),
+            amount=amount,
+            amount_paid=amount,
+            currency=credit.currency,
+            credit=credit,
+            transaction=transaction,
+        )
+
+        # Las señales manejarán la lógica adicional
+        return Response(
+            {"detail": "Abono registrado exitosamente"}, status=status.HTTP_201_CREATED
+        )
 
 class CreditViewSet(viewsets.ModelViewSet):
     queryset = Credit.objects.all()
@@ -190,157 +235,4 @@ class FinanceView(APIView):
                 {"error": str(e)}, 
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
-        
-# # Vista para obtener resumen general
-# class FinanceView(APIView):
-#     def post(self, request):
-#         # Obtén las fechas de inicio y fin del periodo actual desde el cuerpo de la solicitud
-#         current_start_date = request.data.get('start_date')
-#         current_end_date = request.data.get('end_date')
-
-#         # Convertir las fechas de cadena a objetos datetime
-#         current_start_date = datetime.strptime(current_start_date, '%Y-%m-%d')
-#         current_end_date = datetime.strptime(current_end_date, '%Y-%m-%d')
-
-#         # Convertir a fechas aware utilizando la zona horaria de Django
-#         current_start_date = timezone.make_aware(current_start_date)
-#         current_end_date = timezone.make_aware(current_end_date)
-
-#         # Obtener la periodicidad en días desde el cuerpo de la solicitud
-#         periodicity_days = int(request.data.get('periodicity_days', 30))  # Default a 30 días si no se proporciona
-
-#         # Calcular las fechas del periodo anterior usando la periodicidad
-#         previous_start_date = current_start_date - timedelta(days=periodicity_days)
-#         previous_end_date = current_end_date - timedelta(days=periodicity_days)
-
-#         # -------------------- PERIODO ACTUAL --------------------
-
-#         # Ingresos (abonos) en el periodo actual
-#         current_income = AccountMethodAmount.objects.filter(
-#             transaction__transaction_type='income',
-#             transaction__date__range=[current_start_date, current_end_date]
-#         ).aggregate(Sum('amount_paid'))['amount_paid__sum'] or 0  # Sumatoria de los abonos (amount_paid)
-
-#         # Gastos en el periodo actual
-#         current_expense = Expense.objects.filter(
-#             date__range=[current_start_date, current_end_date]
-#         ).aggregate(Sum('amount'))['amount__sum'] or 0
-
-#         # Remaining en el periodo actual
-#         current_remaining = current_income - current_expense
-
-#         # Calcular la sumatoria de los créditos dados en el periodo actual
-#         current_loans = Credit.objects.filter(
-#             first_date_payment__range=[current_start_date, current_end_date]
-#         ).aggregate(Sum('price'))['price__sum'] or 0
-
-#         # Calcular la sumatoria de los saldos pendientes (receivables) en el periodo actual
-#         current_receivables = Credit.objects.filter(
-#             first_date_payment__range=[current_start_date, current_end_date],
-#             pending_amount__gt=0
-#         ).aggregate(Sum('pending_amount'))['pending_amount__sum'] or 0
-
-#         # Ganancias (earnings) en el periodo actual
-#         current_earnings = Credit.objects.filter(
-#             first_date_payment__range=[current_start_date, current_end_date]
-#         ).aggregate(total=Sum('earnings'))['total'] or 0
-
-#         # -------------------- PERIODO ANTERIOR --------------------
-
-#         # Calcular los valores para el periodo anterior
-#         previous_income = AccountMethodAmount.objects.filter(
-#             transaction__transaction_type='income',
-#             transaction__date__range=[previous_start_date, previous_end_date]
-#         ).aggregate(Sum('amount_paid'))['amount_paid__sum'] or 0
-
-#         previous_expense = Expense.objects.filter(
-#             date__range=[previous_start_date, previous_end_date]
-#         ).aggregate(Sum('amount'))['amount__sum'] or 0
-
-#         previous_remaining = previous_income - previous_expense
-
-#         # Calcular la sumatoria de los créditos dados en el periodo anterior
-#         previous_loans = Credit.objects.filter(
-#             first_date_payment__range=[previous_start_date, previous_end_date]
-#         ).aggregate(Sum('price'))['price__sum'] or 0
-
-#         # Calcular la sumatoria de los saldos pendientes (receivables) en el periodo anterior
-#         previous_receivables = Credit.objects.filter(
-#             first_date_payment__range=[previous_start_date, previous_end_date],
-#             pending_amount__gt=0
-#         ).aggregate(Sum('pending_amount'))['pending_amount__sum'] or 0
-
-#          # Ganancias (earnings) en el periodo anterior
-#         previous_earnings = Credit.objects.filter(
-#             first_date_payment__range=[previous_start_date, previous_end_date]
-#         ).aggregate(total=Sum('earnings'))['total'] or 0
-
-#         # -------------------- CAMBIOS PORCENTUALES --------------------
-
-
-#         # Calcular el porcentaje de cambio
-#         def calculate_percentage_change(current, previous):
-#             if previous == 0:
-#                 return 100 if current > 0 else 0
-#             return ((current - previous) / previous) * 100
-
-#         income_change = calculate_percentage_change(current_income, previous_income)
-#         expense_change = calculate_percentage_change(current_expense, previous_expense)
-#         remaining_change = calculate_percentage_change(current_remaining, previous_remaining)
-#         loans_change = calculate_percentage_change(current_loans, previous_loans)
-#         receivables_change = calculate_percentage_change(current_receivables, previous_receivables)
-#         earnings_change = calculate_percentage_change(current_earnings, previous_earnings)
-
-#         return Response({
-#             'current': {
-#                 'income': current_income,
-#                 'expense': current_expense,
-#                 'remaining': current_remaining,
-#                 'loans': current_loans,
-#                 'receivables': current_receivables,
-#                  'earnings': float(current_earnings),
-#             },
-#             'previous': {
-#                 'income': previous_income,
-#                 'expense': previous_expense,
-#                 'remaining': previous_remaining,
-#                 'loans': previous_loans,
-#                 'receivables': previous_receivables,
-#                 'earnings': float(previous_earnings),
-#             },
-#             'change': {
-#                 'income_change': income_change,
-#                 'expense_change': expense_change,
-#                 'remaining_change': remaining_change,
-#                 'loans_change': loans_change,
-#                 'receivables_change': receivables_change,
-#                 'earnings_change': earnings_change,
-#             }
-#         }, status=status.HTTP_200_OK)
-
-
-   
-#     def post(self, request, *args, **kwargs):
-#         # Obtener las fechas del cuerpo
-#         start_date = parse_date(request.data.get('start_date'))
-#         end_date = parse_date(request.data.get('end_date'))
-
-#         if not start_date or not end_date:
-#             return Response({"error": "start_date y end_date son requeridos."}, status=status.HTTP_400_BAD_REQUEST)
-
-#         # Filtrar los créditos financieros en el período
-#         # credits = Credit.objects.filter(subcategory='financial_product', created_at__range=[start_date, end_date])
-#         credits = Credit.objects.filter(subcategory_id=1, created_at__range=[start_date, end_date])
-
-#         # Instanciar el paginador
-#         # paginator = CustomOrderPagination()
-
-#         # Aplicar paginación a los resultados
-#         # paginated_credits = paginator.paginate_queryset(credits, request)
-
-#         # Usar el serializer para todos los datos
-#         serializer = CreditSerializer(credits, many=True)
-
-#         # Devolver los datos paginados
-#         # return paginator.get_paginated_response(serializer.data)
-#         return serializer.data
+    
