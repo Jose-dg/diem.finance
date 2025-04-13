4ad333c59dfba8f00ac24d6b498601b8a7f9e4f2
jose <jose.ojeda.k@hotmail.com>
2025-04-12 20:54:08 -0500

    Update new funtion 12/4/25 3.0

--- apps/fintech/utils.py
+++ apps/fintech/utils.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from django.db.models import Sum
 from decimal import Decimal
 from django.utils import timezone
-from models import Credit, CreditAdjustment  
+from models import Credit  
 from django.db import transaction
 
 def calculate_credit_morosity(credit):
