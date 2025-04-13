a5818381dd53398e81a11bdd4cf22cdb7b7f32a8
jose <jose.ojeda.k@hotmail.com>
2025-04-12 20:49:23 -0500

    Update new funtion 12/4/25 2.0

--- apps/fintech/signals.py
+++ apps/fintech/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 
-from apps.fintech.utils import calculate_credit_morosity, recalculate_credit_pending_amount
+from apps.fintech.utils import calculate_credit_morosity, recalculate_credit, recalculate_credit_pending_amount
 from .models import Transaction, AccountMethodAmount, Credit
 from datetime import datetime
 from django.db.models.signals import post_save
