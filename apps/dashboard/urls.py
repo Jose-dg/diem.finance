ea332f0f1b3ca596ec50e6b40e6dda7756c64bea
jose <jose.ojeda.k@hotmail.com>
2025-05-26 22:05:36 -0500

    Deleting data non-funtional 2.0

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -4,19 +4,15 @@ from .views import (
    FinanceView,
    SellerChartDataAPIView,
    TransactionsAPIView, 
-   ClientsWithDefaultAPIView,
-   SortedCreditsByLabelAPIView,
    MonthlyChartDataAPIView
    )
 
 urlpatterns = [
     path('credits/', CreditsAPIView.as_view(), name='credits'),
     path('transactions/', TransactionsAPIView.as_view(), name='transactions'),
-    path('clients/defaults/', ClientsWithDefaultAPIView.as_view(), name='clients_with_default'),
     
     # Rutas para APIViews
     path('data/', FinanceView.as_view(), name='finance-summary'),
-    path('sheet/', SortedCreditsByLabelAPIView.as_view(), name='creditw'),
     path("chart/sellers/", SellerChartDataAPIView.as_view(), name="chart-sellers"),
     path("chart/monthly/", MonthlyChartDataAPIView.as_view(), name="monthly-chart-data")
 ]
