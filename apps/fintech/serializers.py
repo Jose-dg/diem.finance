6b649447cc4277552a3c9e25fb7d90d92b7a4d4d
jose <jose.ojeda.k@hotmail.com>
2025-06-23 23:33:29 -0500

    New filter view for filter 2.0

--- apps/fintech/serializers.py
+++ apps/fintech/serializers.py
@@ -217,4 +217,3 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
-    max_page_size = 100
